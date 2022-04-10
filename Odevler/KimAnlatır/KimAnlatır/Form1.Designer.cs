2cef2444b40843e5a2845039e9ef126dcc573653
tubakaradut <karadut.tuba@gmail.com>
2022-04-10 18:43:08 +0300

    Odevler Eklendi.

--- Odevler/KimAnlatır/KimAnlatır/Form1.Designer.cs
+++ Odevler/KimAnlatır/KimAnlatır/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace KimAnlatır
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // SınıfListesi
@@ -60,7 +61,7 @@ namespace KimAnlatır
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(481, 399);
+            this.button1.Location = new System.Drawing.Point(423, 399);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(208, 72);
             this.button1.TabIndex = 2;
@@ -93,11 +94,22 @@ namespace KimAnlatır
             this.columnHeader2.Text = "Konu";
             this.columnHeader2.Width = 202;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(673, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 72);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 523);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Konular);
@@ -117,6 +129,7 @@ namespace KimAnlatır
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
